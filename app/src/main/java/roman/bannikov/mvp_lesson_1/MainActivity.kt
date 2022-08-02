c8d544e684fd020f7b9015ad7fa23cd4854c41d4
ThePhyrus <romaisgood@yandex.ru>
2022-08-02 15:03:13 +0300

    Initial commit

---
+++ app/src/main/java/roman/bannikov/mvp_lesson_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package roman.bannikov.mvp_lesson_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
