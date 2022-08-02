e54ef46905ff20332872343fec1a1edde181107f
ThePhyrus <romaisgood@yandex.ru>
2022-08-02 15:33:33 +0300

    Урок 1. MVP. Подключил viewBinding, набросал разметку activity_main.

--- app/src/main/java/roman/bannikov/mvp_lesson_1/MainActivity.kt
+++ app/src/main/java/roman/bannikov/mvp_lesson_1/MainActivity.kt
@@ -1,11 +1,16 @@
 package roman.bannikov.mvp_lesson_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import roman.bannikov.mvp_lesson_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
